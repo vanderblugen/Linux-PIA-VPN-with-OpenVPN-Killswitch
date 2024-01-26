1cbf7ae0320b1fcc754298fd6968bf4454d85463
vanderblugen <33649889+vanderblugen@users.noreply.github.com>
2024-01-25 21:38:39 -0600

    Update ip_tables.php

--- ip_tables.php
+++ ip_tables.php
@@ -4,6 +4,8 @@
 // This script installs PIA VPN using OpenVPN and sets up a killswitch using iptables
 // This script needs to be run as root.
 
+// Setup the variables
+
 // PIA Credentials
 $pia_username = "";
 $pia_password = "";
@@ -27,53 +29,55 @@ $port2_type="UDP";
 // Name of country filename
 $filename="CA Toronto.ovpn";
 
+// Some variables
+$openvpn_location = "/etc/openvpn/";
+$vpn_config_filename = "vpn.conf";
+$credential_filename = "login";
+$old_pattern = "auth-user-pass";
+$new_pattern = "auth-user-pass /etc/openvpn/$(credential_filename)";
+$local_zip_file = "openvpn-strong.zip";
+$url = "https://www.privateinternetaccess.com/openvpn/openvpn-strong.zip";
+
 // Verify if running as root
 if (posix_getuid() !== 0){
         exit ("This is non-root");
     };
 
 // Run updates
-echo shell_exec('apt update -y');
-echo shell_exec('apt upgrade -y');
+#echo shell_exec('apt update -y');
+#echo shell_exec('apt upgrade -y');
 
 // Install needed software
-echo shell_exec('apt-get install -f ifupdown openvpn resolvconf php-zip -y');
+#echo shell_exec('apt-get install -f ifupdown openvpn resolvconf php-zip -y');
 
 // Restart resolvconf service
-echo shell_exec("systemctl start resolvconf.service");
+#echo shell_exec("systemctl start resolvconf.service");
 
 // Get VPN files
-$local_zip_file = "openvpn-strong.zip";
-$url = "https://www.privateinternetaccess.com/openvpn/openvpn-strong.zip";
-$zip_file = file_get_contents($url);
-file_put_contents($local_zip_file, $zip_file);
-
-// Unzip the file into the appropriate directory
-$unzip_location = "/etc/openvpn";
+$output = file_get_contents($url);
+file_put_contents($local_zip_file, $output);
 
 $zip = new ZipArchive;
 if ($zip->open($local_zip_file) === TRUE) {
-    $zip->extractTo($unzip_location);
+    $zip->extractTo($openvpn_location);
     $zip->close();
-    echo ("Unzipped $local_zip_file into $unzip_location\n");
+    echo ("Unzipped $local_zip_file into $openvpn_location\n");
 } else {
     exit ("Exiting script.\nUnzip failed\n");
 }
 
 // Delete the zip file
 if (!unlink($local_zip_file)) { 
-    exit ("Exiting script.\nUnable to delete $local_zip_file");
+    exit ("Exiting script.\nUnable to delete $local_zip_file\n");
 } 
 else { 
     echo ("$local_zip_file has been deleted\n"); 
 }
 
 // Change directory
-$openvpn_location = "/etc/openvpn";
 chdir($openvpn_location);
 
 // Create credential file 
-$credential_filename = "login";
 $file = fopen($credential_filename, "w");
 
 if ($file) {
@@ -90,16 +94,13 @@ if ($file) {
 }
 
 // Update permissions on the credential file
-chmod("$openvpn_location/$credential_filename", 0500);
+chmod($credential_filename, 0500);
 
 // Copy the openvpn file into the appropriate filename
-$vpn_config_filename = "vpn.conf";
-copy($filename, $vpn_config_filename);
+move_uploaded_file($filename, $vpn_config_filename);
 
-$old_pattern = "auth-user-pass";
-$new_pattern = "auth-user-pass /etc/openvpn/$(credential_filename)";
+// Update $vpn_config_filename to have login credential file
 $output = preg_replace($old_pattern, $new_pattern, $vpn_config_filename);
-file_put_contents($vpn_config_filename, $output);
-
+file_put_contents($credential_filename, $output);
 
 ?>
